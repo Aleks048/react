e24ea9e04b98da01cd31c914b121b93642c5d06a
Aleks048 <ashum048@uottawa.ca>
2019-08-24 16:38:38 -0400

    removing

--- ch14_redux/src/modules/index.js
+++
@@ -1,6 +0,0 @@
-const {combineReducers} = require("redux")
-
-const {reducer:movies} = require("./movies")//create a reducer object called movies from movies.js
-
-
-module.expoorts = combineReducers({movies})
